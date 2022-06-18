7d34c40fa32bad2c675a50888f93fac984499270
Damien Jones <damienjonescore@gmail.com>
2022-06-18 22:31:03 +0100

    Adding missing file to V1 folder

---
+++ EDMTracksLosslessS3Upload-V1/EDMTracksLosslessS3Upload-VariablesBlank.ps1
@@ -0,0 +1,18 @@
+#Set Variables
+
+
+#The local file path for objects to upload to S3
+#E.g. "C:\Users\Files\"
+$LocalSource =
+
+#The S3 bucket to upload the objects to
+#E.g. "my-s3-bucket"
+$S3BucketName =
+
+#The S3 bucket prefix / folder to upload the objects to (if applicable)
+#E.g. "Folder\SubFolder\"
+$S3KeyPrefix =
+
+#The S3 Storage Class to upload to
+#E.g. "GLACIER"
+$S3StorageClass =
